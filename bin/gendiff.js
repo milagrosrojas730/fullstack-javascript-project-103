55ba7550363696b1e23b2f7a6848ac9063612c3e
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-11-25 13:06:55 -0500

    Adding comments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command }  from ('commander');
+import genDiff from '../src/index.js';
 const program = new Command();
 
 program
   .version('1.0.0') 
-  .description('Compares two configuration files and shows a difference.') 
-  .helpOption('-h, --help', 'Output usage information'); 
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<file1>', 'first configuration file')
+  .argument('<file2>', 'second configuration file')
+  .option('-h, --help', 'Output usage information')
+  .action((file1, file2, options) => {
+    const diff = genDiff(file1, file2, options.format);
+    console.log(diff);
+  });
 
-program.parse(); 
+program.parse(process.argv); 
