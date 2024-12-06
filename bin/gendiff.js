3d7b9bf212aaa5536a1affde049719a8c9c94d13
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-12-06 17:43:43 -0500

    Adding make commands

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-import { Command }  from ('commander');
+import { Command } from 'commander';
 import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .version('1.0.0') 
   .description('Compares two configuration files and shows a difference.')
-  .argument('<file1>', 'first configuration file')
-  .argument('<file2>', 'second configuration file')
-  .option('-h, --help', 'Output usage information')
-  .action((file1, file2, options) => {
-    const diff = genDiff(file1, file2, options.format);
+  .argument('<filepath1>', 'path to the first configuration file')
+  .argument('<filepath2>', 'path to the second configuration file')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format (default: stylish)', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   });
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import genDiff from '..src/index.js';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import yaml from 'js-yaml';
+
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const readFile = (filepath) => {
+    const ext = path.extname(filepath);
+    if( ext === '.json') {
+        return JSON.parse(fs.readFileSync(filepath, 'utf-8'));
+    }
+    if(ext === '.yml'|| ext === '.yaml') {
+        return yaml.load(fs.readFileSync(filepath, 'utf-8'));
+    }
+    throw new Error('Unsupported file format');
+}
+const file1 = readFile(filepath1);
+const file2 = readFile(filepath2);
+const allKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+
+const diffLines = allKeys.map((key) => {
+    if (!_.has(file2, key)) {
+    return `  - ${key}: ${file1[key]}`;
+    }
+    if (!_.has(file1, key)) {
+    return `  + ${key}: ${file2[key]}`;
+    }
+    if (!_.isEqual(file1[key], file2[key])) {
+    return [
+        `  - ${key}: ${file1[key]}`,
+        `  + ${key}: ${file2[key]}`,
+    ].join('\n');
+    }
+    return `    ${key}: ${file1[key]}`;
+});
+
+
+return `{\n${diffLines.join('\n')}\n}`;
+};
 
 export default genDiff;
