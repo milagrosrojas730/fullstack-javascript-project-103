b7556fc0cbf75aa46f5e5d1256f045454d6dd956
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-12-20 17:09:40 -0500

    Adding make commands

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -2,11 +2,13 @@ import fs from 'fs';
 import getPath from '../src/getFixturePath.js';
 import genDiff from '../src/index.js';
 
-const plainResult = fs.readFileSync(getPath('result.txt'), 'utf8');
+const styres = fs.readFileSync(getPath('stylish.txt'), 'utf8');
+const result = fs.readFileSync(getPath('result.txt'), 'utf8');
+const jsoret = JSON.parse(fs.readFileSync(getPath('js.json'), 'utf8'));
 
-const generateFilePaths = (filename1, filename2, extension) => [
-  getPath(`${filename1}.${extension}`),
-  getPath(`${filename2}.${extension}`),
+const generateFilePaths = (filename1, filename2, extens) => [
+  getPath(`${filename1}.${extens}`),
+  getPath(`${filename2}.${extens}`),
 ];
 
 const testGenDiff = (filepath1, filepath2, format, expectedResult) => {
@@ -19,10 +21,15 @@ const testGenDiffJSON = (filepath1, filepath2, expectedResult) => {
 
 test('Format for stylish Result - YAML File', () => {
   const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
-  testGenDiff(filepath1, filepath2, 'stylish', stylishResult);
+  testGenDiff(filepath1, filepath2, 'stylish', styres);
 });
 
-test('Format for plain Result - YAML File', () => {
+test('Format for Result - YAML File', () => {
   const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
-  testGenDiff(filepath1, filepath2, 'plain', plainResult);
+  testGenDiff(filepath1, filepath2, 'plain', result);
+});
+
+test('Format for JSON Result - JSON File', () => {
+  const [filepath1, filepath2] = generateFilePaths('file1', 'file2', 'json');
+  testGenDiffJSON(filepath1, filepath2, jsoret);
 });
--- src/utils.js
+++ src/utils.js
@@ -8,10 +8,8 @@ const CHANGED_VALUE = 'changed';
 const ROOT_VALUE = 'root';
 
 const getIndentation = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
-
 const formatValue = (data, depth, renderFns) => {
   if (!_.isObject(data)) return String(data);
-
   const entries = Object.entries(data).map(([key, value]) => renderFns[UNCHANGED_VALUE]({ key, value }, depth + 1, formatValue));
 
   return `{\n${entries.join('\n')}\n${getIndentation(depth)}  }`;
@@ -21,16 +19,13 @@ const renderFns = {
     const renderedChildren = children.flatMap((child) => iterate(child, depth + 1, iterate));
     return `{\n${renderedChildren.join('\n')}\n}`;
   },
-
   [NESTED_VALUE]: ({ key, children }, depth, iterate) => {
     const nestedChildren = children.flatMap((child) => iterate(child, depth + 1, iterate));
     return `${getIndentation(depth)}  ${key}: {\n${nestedChildren.join('\n')}\n${getIndentation(depth)}  }`;
   },
-
   [ADD_VALUE]: (node, depth) => `${getIndentation(depth)}+ ${node.key}: ${formatValue(node.value, depth, renderFns)}`,
   [DELETED_VALUE]: (node, depth) => `${getIndentation(depth)}- ${node.key}: ${formatValue(node.value, depth, renderFns)}`,
   [UNCHANGED_VALUE]: (node, depth) => `${getIndentation(depth)}  ${node.key}: ${formatValue(node.value, depth, renderFns)}`,
-
   [CHANGED_VALUE]: (node, depth) => {
     const { key, value1, value2 } = node;
     const formattedValue1 = `${getIndentation(depth)}- ${key}: ${formatValue(value1, depth, renderFns)}`;
@@ -38,9 +33,7 @@ const renderFns = {
     return [formattedValue1, formattedValue2].join('\n');
   },
 };
-
 const formatNode = (node, depth) => renderFns[node.type](node, depth, formatNode);
-
 const stylish = (diff) => formatNode(diff, 0);
 const stylishWithBraces = (diff) => {
   const innerOutput = stylish(diff);
