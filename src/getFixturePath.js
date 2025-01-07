ef89412dc36859c39e17b70b4d356ae806047cb7
Clara Rojas Levano <milagrosrojas730@gmail.com>
2025-01-07 11:40:02 -0500

    Adding make commands

--- src/getFixturePath.js
+++ src/getFixturePath.js
@@ -1,5 +1,8 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const getPath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
 export default getPath;
