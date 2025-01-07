0d5cd899905cb556f1b35fe95e483accf9f25c06
Clara Rojas Levano <milagrosrojas730@gmail.com>
2025-01-07 09:22:34 -0500

    Adding make commands

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,35 +1,55 @@
 import fs from 'fs';
-import getPath from '../src/getFixturePath.js';
+import getPath from '../__test__/getFixturePath.test.js';
 import genDiff from '../src/index.js';
 
 const styres = fs.readFileSync(getPath('stylish.txt'), 'utf8');
 const result = fs.readFileSync(getPath('result.txt'), 'utf8');
 const jsoret = JSON.parse(fs.readFileSync(getPath('js.json'), 'utf8'));
-
 const generateFilePaths = (filename1, filename2, extens) => [
   getPath(`${filename1}.${extens}`),
   getPath(`${filename2}.${extens}`),
 ];
 
 const testGenDiff = (filepath1, filepath2, format, expectedResult) => {
-  expect(genDiff(filepath1, filepath2, format)).toBe(expectedResult);
+  expect(genDiff(filepath1, filepath2, format)).toEqual(expectedResult);
 };
 
 const testGenDiffJSON = (filepath1, filepath2, expectedResult) => {
   expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedResult);
 };
 
+const validateFileContent = (content, format) => {
+  try {
+    if(format === 'json') {
+      JSON.parse(content);
+    }
+    return true;
+  } catch (error) {
+    console.error(`Invalid content for format ${format}:`, error.message);
+    return false;
+  }
+};
+
 test('Format for stylish Result - YAML File', () => {
   const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
+  const output = genDiff(filepath1, filepath2, 'stylish');
+  console.log('Stylish output:', output);
+  expect(validateFileContent(output, 'stylish')).toBe(true);
   testGenDiff(filepath1, filepath2, 'stylish', styres);
 });
 
 test('Format for Result - YAML File', () => {
   const [filepath1, filepath2] = generateFilePaths('file1-y', 'file2-y', 'yaml');
+  const output = genDiff(filepath1, filepath2, 'plain');
+  console.log('Plain Output:', output);
+  expect(validateFileContent(output, 'plain')).toBe(true);
   testGenDiff(filepath1, filepath2, 'plain', result);
 });
 
 test('Format for JSON Result - JSON File', () => {
   const [filepath1, filepath2] = generateFilePaths('file1', 'file2', 'json');
+  const output = genDiff(filepath1, filepath2, 'json');
+  console.log('JSON Output:', output)
+  expect(validateFileContent(output, 'json')).toBe(true);
   testGenDiffJSON(filepath1, filepath2, jsoret);
 });
