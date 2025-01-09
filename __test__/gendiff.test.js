c4c09b37f25dac337525c4d09473c6a7bbb1c5bc
Clara Rojas Levano <milagrosrojas730@gmail.com>
2025-01-09 18:48:23 -0500

    Adding make commands

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,29 +1,27 @@
 import fs from 'fs';
-import getPath from '../src/getFixturePath.js';
 import genDiff from '../src/index.js';
 import getFixturePath from '../src/getFixturePath.js';
 
-const styres = fs.readFileSync(getPath('stylish.txt'), 'utf8');
-const result = fs.readFileSync(getPath('result.txt'), 'utf8');
-const jsoret = JSON.parse(fs.readFileSync(getPath('js.json'), 'utf8'));
+const styres = fs.readFileSync(getFixturePath('stylish.txt'), 'utf8');
+const result = fs.readFileSync(getFixturePath('result.txt'), 'utf8');
+const jsoret = JSON.parse(fs.readFileSync(getFixturePath('js.json'), 'utf8'));
 
 const generateFilePaths = (filename1, filename2, extens) => [
-  getPath(`${filename1}.${extens}`),
-  getPath(`${filename2}.${extens}`),
+  getFixturePath(`${filename1}.${extens}`),
+  getFixturePath(`${filename2}.${extens}`),
 ];
 const testGenDiff = (filepath1, filepath2, format) => {
   expect(genDiff(filepath1, filepath2, format));
 };
 
-const testGenDiffJSON = (filepath1, filepath2) => {
-  expect(JSON.parse(genDiff(filepath1, filepath2, 'json')));
+const testGenDiffJSON = (filepath1, filepath2, expectedResult) => {
+  expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedResult);
 };
 
 test('Format for stylish Result - YAML File', () => {
   const filepath1 = getFixturePath('file1.yaml');
   const filepath2 = getFixturePath('file2.yaml');
-  const expectedResult = fs.readFileSync(getFixturePath('stylish.txt'), 'utf8');
-  expect(genDiff(filepath1, filepath2, 'stylish'));
+  expect(genDiff(filepath1, filepath2, 'stylish', styres));
 });
 
 test('Format for Result - YAML File', () => {
